096faa6e1191e10a3d675a6d4f6a747a11518b1f
masterpicho <a01374529@itesm.mx>
2019-06-27 17:03:56 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,22 @@
-<?
+<?PHP
 $conn=new mysqli("localhost", "registro", "", "registro") or die("ConnectionFailed");
 
-if(isset($_POST['login'])){ //check if form was submitted
-  $usuario = $_POST['usuario']; //get input text
-  $psw = $_POST['contrasena'];
+$this->pdo = new PDO($GLOBALS['localhost'], $GLOBALS['registro'], $GLOBALS['']);
+
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $this->pdo->query("CREATE TABLE users (usuario VARCHAR(50) NOT NULL, contrasena VARCHAR(200) NOT NULL)");
+
+$usuario="Luis";
+$contrasenaEncriptada=password_hash("Hola", PASSWORD_DEFAULT);
+    $queryIngresarUsuario="INSERT INTO `users`(`usuario`, `contrasena`) VALUES ('$usuario','$contrasenaEncriptada')";
+    $resultIngresarUsuario=mysqli_query($conn,$queryIngresarUsuario);
+
+//check if form was submitted
+  //$usuario = $_POST['usuario']; //get input text
+ // $psw = $_POST['contrasena'];
   $query1 = "SELECT * FROM users WHERE usuario = '$usuario'";
   $result = mysqli_query($conn, $query1);
-  $message="";
   if(mysqli_num_rows($result)!=0){
   while($data= mysqli_fetch_assoc($result) ) {
 				//	$IDUsuario = $data['ID'];
@@ -19,18 +29,7 @@ if(isset($_POST['login'])){ //check if form was submitted
                    // $psw=password_verify($psw, $contrasena);
 					}
   if(password_verify($psw, $contrasena)){
-  //$sesionIniciada=true;
-  
-  //$message = "Bienvenido ".$nombre;
-  //$_SESSION['IDusuario']=$IDUsuario;
-//  $_SESSION['nombre']=$nombreUsuario;
-  //$_SESSION['apellido']=$apellidoUsuario;
-//  $_SESSION['sesioniniciada']=true;
-  //$_SESSION['tipo_usuario']=$tipo_usuario;
-  //$message = "Bienvenido ".$_SESSION['nombre'];
-//  $_SESSION['mensajeBienvenida']=$message;
- //header("Location: pagosyadeudos.php");
- 
+
 print("Sesion Iniciada <br>");
 print($contrasena);
   }
@@ -38,27 +37,5 @@ print($contrasena);
   }
   
   
-}
-?>
 
-<!DOCTYPE html>
-<html lang="en-us">
-  <head>
-      <style>
-          body{
-              background-color:#0099ff;
-          }
-      </style>
-    <meta charset="utf-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Prueba Tecmi</title>
-  
-  </head>
-  <body>
-    <form action="" method="post">
-        <input type="text" name="usuario" value="">
-        <input type="password" name="contrasena" value="">
-        <input type="submit" name="login" value="Crear Usuario">
-    </form>
-  </body>
-</html>
+?>
